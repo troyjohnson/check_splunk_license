5c91a87e2bae6a6c2e60e3fc2af4b6e3cf764832
Troy Johnson <troy@jdmz.net>
2012-04-16 23:18:05 -0500

    Added exclusion for RolloverSummary type to search (thanks hexx0)

--- check_splunk_license.pl
+++ check_splunk_license.pl
@@ -105,7 +105,7 @@ if ($warning !~ m/^[\d]+$/) {
 }
 
 # get Splunk license information
-my $search_string = "\'host=${host} index=_internal source=*/license_usage.log earliest_time=-0d\@d latest_time=now | eval mb=b/1024/1024 | stats sum(mb) as _raw\'";
+my $search_string = "\'host=${host} index=_internal source=*/license_usage.log earliest_time=-0d\@d latest_time=now type!=RolloverSummary | eval mb=b/1024/1024 | stats sum(mb) as _raw\'";
 my $options = "-batch true -preview false -output rawdata -auth ${user}:${password} -uri https://${host}:${splunk_port}";
 my $output = `$splunk $splunk_command $search_string $options 2>/dev/null`;
 
